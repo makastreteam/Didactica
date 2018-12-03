61b1ac7427ac41e299945620520532d65a1a2064
danzel92 <33261967+danzel92@users.noreply.github.com>
2018-12-03 21:13:32 +0100

    Mapa
    Se ha añadido el mapa y  el movimiento del goblin por el mismo

---
+++ Assets/Scripts/MovePoints.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+/// <summary>
+/// State in which the enemy goes through its patrol points.
+/// When transitioning back to this state, the enemy continues its patrol route where he left of.
+/// </summary>
+public class MovePoints: MonoBehaviour{
+
+	public GameObject[] Points;
+
+    int index = 0;
+    float minDist = 0.01f;
+    float speed = 1;
+    bool start = true;
+
+    public void Move(Transform target)
+    {
+        //transform.LookAt(new Vector3(Points[index].transform.position.x, transform.position.y, Points[index].transform.position.z));
+        transform.position =Vector3.MoveTowards(transform.position, target.position,speed * Time.deltaTime);
+    }
+
+    IEnumerator RotateTowards(Transform target)
+    {
+        Vector3 direction = new Vector3(target.position.x - transform.position.x, target.position.y - transform.position.y,  transform.position.z);
+        direction.z = 0;
+        Quaternion toRotation = Quaternion.FromToRotation(transform.rotation.eulerAngles, direction);
+        float timer = 0.4f;
+        while ((timer -= Time.deltaTime) > 0)
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction), speed * Time.deltaTime);
+
+            yield return null;
+        }
+    }
+
+    void Update ()
+    {
+        if(start == true)
+        {
+            float dist = Vector3.Distance(transform.position, Points[index].transform.position);
+
+            if (dist > minDist)
+            {
+                Move(Points[index].transform);
+                //StartCoroutine(RotateTowards(Points[index].transform));
+            }
+            else
+            {
+                if (index + 1 == Points.Length)
+                {
+                    index = 0;
+                }
+                else
+                {
+                    index++;
+                }
+            }
+        }
+	}
+
+	public void OnStateEnter ()
+    {
+        start = true;
+	}
+
+	public void OnStateLeave ()
+    {
+        index = 1;
+        start = false;
+    }
+
+}
---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+public class Tile : MonoBehaviour {
+
+	void OnMouseDown(){
+		//Pawn.instance.transform.LookAt(transform.position);
+		Pawn.instance.transform.DOMove(transform.position,0.5f).SetEase(Ease.InOutQuad);
+		
+	}
+}
