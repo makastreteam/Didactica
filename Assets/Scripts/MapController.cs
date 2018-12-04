5ccb9ba7314ef4b54c3b6645a8f90c6cc85e68d5
danzel92 <33261967+danzel92@users.noreply.github.com>
2018-12-04 22:57:35 +0100

    Creacion de la escena Game_Alquimia

---
+++ Assets/Scripts/MapController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MapController : MonoBehaviour {
+
+	public void Play()
+    {
+        SceneManager.LoadScene("Game_Alquimia", LoadSceneMode.Single);
+    }
+
+}
