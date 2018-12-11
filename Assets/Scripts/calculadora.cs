8cfee3575f05e09d7dcee31093b8be42a2c89e01
danzel92 <33261967+danzel92@users.noreply.github.com>
2018-12-11 21:10:56 +0100

    Game Alquimia
    Ahora funcionan los botones y calcula la suma, falta hacer que los numeros y operadores sean random y subir el nivel

---
+++ Assets/Scripts/calculadora.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class calculadora : MonoBehaviour {
+
+    public Text resultado;
+    public Text lblvalor1;
+    public Text lblvalor2;
+    public Text operacion;
+    private string textoimprimir;
+    string valor1;
+    string valor2;
+    string operacionActual;
+    double rta;
+    int nivel = 0;
+    int curacion;
+    Stats gameStats;
+    private void Start()
+    {
+        gameStats = GameObject.FindGameObjectWithTag("Stats").GetComponent<Stats>();
+        curacion = gameStats.GetPlayerHealth();
+    }
+
+    public void BorrarC()
+    {
+
+        resultado.text = "?";
+    }
+
+    IEnumerator MostrarVida()
+    {
+            yield return new WaitForSeconds (2);
+            Debug.Log("Funciona");
+    }
+
+    IEnumerator NextLevel()
+    {
+        yield return new WaitForSeconds(2);
+        Debug.Log("Funciona");
+    }
+
+    public void igual()
+    {
+        operacionActual = operacion.text;
+        valor1 = lblvalor1.text;
+        valor2 = lblvalor2.text;
+        if (resultado.text != "?")
+        {
+            if (resultado.text == operaciones(valor1, valor2)) {
+                nivel += 1;
+                curacion += 25;
+                Debug.Log("vida++");
+                if (nivel == 3)
+                {
+                    StartCoroutine("MostrarVida");
+                    gameStats.SetPlayerHealth(curacion);
+                }
+                else
+                {
+                    StartCoroutine("NextLevel");
+                }
+                
+            }
+            else{
+                Debug.Log("Trabaja");
+                StartCoroutine("MostrarVida");
+            }
+        }
+    }
+
+    public string operaciones(string n1, string n2)
+    {
+        string respuesta = "";
+        if ("+" == operacion.text)
+        {
+
+            respuesta = (int.Parse(n1) + int.Parse(n2)).ToString();
+        }
+        else if ("-"==operacion.text){
+            respuesta = (int.Parse(n1) - int.Parse(n2)).ToString();
+        }
+        else
+        {
+            respuesta = (int.Parse(n1) * int.Parse(n2)).ToString();
+        }
+        return respuesta;
+    }
+}
---
+++ Assets/Scripts/escribirNumeros.cs
@@ -0,0 +1,130 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class escribirNumeros : MonoBehaviour {
+
+    private string textoimprimir;
+    public Text resultado;
+    private int contador=0;
+
+    public void colocar0()
+    {
+        interrogacion();
+        if (contador != 2)
+        {
+            textoimprimir = "0";
+            resultado.text = resultado.text + textoimprimir;
+            contador++;
+        }
+    }
+    public void colocar1()
+    {
+        interrogacion();
+        if (contador != 2)
+        {
+            textoimprimir = "1";
+            resultado.text = resultado.text + textoimprimir;
+            contador++;
+        }
+    }
+    public void colocar2()
+    {
+        interrogacion();
+        if (contador != 2)
+        {
+            textoimprimir = "2";
+            resultado.text = resultado.text + textoimprimir;
+            contador++;
+        }
+    }
+    public void colocar3()
+    {
+        interrogacion();
+        if (contador != 2)
+        {
+            textoimprimir = "3";
+            resultado.text = resultado.text + textoimprimir;
+            contador++;
+        }
+    }
+    public void colocar4()
+    {
+        interrogacion();
+        if (contador != 2)
+        {
+            textoimprimir = "4";
+            resultado.text = resultado.text + textoimprimir;
+            contador++;
+        }
+    }
+    public void colocar5()
+    {
+        interrogacion();
+        if (contador != 2)
+        {
+            textoimprimir = "5";
+            resultado.text = resultado.text + textoimprimir;
+            contador++;
+        }
+    }
+    public void colocar6()
+    {
+        interrogacion();
+        if (contador != 2)
+        {
+            textoimprimir = "6";
+            resultado.text = resultado.text + textoimprimir;
+            contador++;
+        }
+    }
+    public void colocar7()
+    {
+        interrogacion();
+        if (contador != 2)
+        {
+            textoimprimir = "7";
+            resultado.text = resultado.text + textoimprimir;
+            contador++;
+        }
+    }
+    public void colocar8()
+    {
+        interrogacion();
+        if (contador != 2)
+        {
+            textoimprimir = "8";
+            resultado.text = resultado.text + textoimprimir;
+            contador++;
+        }
+    }
+    public void colocar9()
+    {
+        interrogacion();
+        if (contador !=2 ) {
+            textoimprimir = "9";
+            resultado.text = resultado.text + textoimprimir;
+            contador++;
+        }
+    }
+
+
+    void interrogacion()
+    {
+        if (resultado.text == "?")
+        {
+            contador = 0;
+            resultado.text = "";
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
